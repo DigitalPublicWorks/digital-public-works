c0213811be36005ee555aaf562725496469a5b64
Steve Johnson <steve@abstractcoder.com>
2019-10-29 14:52:45 -0500

    Modified config for Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :digital_public_works, DigitalPublicWorksWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "digital-public-works", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
--- lib/digital_public_works_web/endpoint.ex
+++ lib/digital_public_works_web/endpoint.ex
@@ -2,7 +2,7 @@ defmodule DigitalPublicWorksWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :digital_public_works
 
   socket "/socket", DigitalPublicWorksWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
