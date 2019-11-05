796666c6deec8869951859952de3f155f715abdd
Steve Johnson <steve@abstractcoder.com>
2019-11-05 09:32:28 -0600

    Switched users to binary_id

--- priv/repo/migrations/20191101152607_create_users.exs
+++ priv/repo/migrations/20191101152607_create_users.exs
@@ -2,7 +2,8 @@ defmodule DigitalPublicWorks.Repo.Migrations.CreateUsers do
   use Ecto.Migration
 
   def change do
-    create table(:users) do
+    create table(:users, primary_key: false) do
+      add :id, :binary_id, primary_key: true
       add :email, :string
       add :password_hash, :string
 
