f416c9f8df11509fba0d817c05eae93942035a5d
Steve Johnson <steve@abstractcoder.com>
2020-01-16 20:32:56 -0600

    Added placeholders for terms and privacy pages

--- lib/digital_public_works_web/controllers/page_controller.ex
+++ lib/digital_public_works_web/controllers/page_controller.ex
@@ -4,4 +4,12 @@ defmodule DigitalPublicWorksWeb.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  def terms(conn, _params) do
+    render(conn, "terms.html")
+  end
+
+  def privacy(conn, _params) do
+    render(conn, "privacy.html")
+  end
 end
