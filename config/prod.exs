2ac723874ed05a44dc80df3a430cc70987da37b8
Nick Gartmann <nick@rokkincat.com>
2020-01-25 10:51:15 -0600

    Change endpoint host to stop it redirecting to digital-public-works

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :digital_public_works, DigitalPublicWorksWeb.Endpoint,
-  url: [scheme: "https", host: "digital-public-works", port: 443],
+  url: [scheme: "https", host: "digitalpublicworks.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
