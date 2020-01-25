17e16159817ca8ad9711c8aa0d4efe94c78c3182
Nick Gartmann <nick@rokkincat.com>
2020-01-25 14:02:34 -0600

    Config for staging environment (#9)
    * Add config for the staging environment
    
    * Add config for the staging environment

---
+++ config/staging.exs
@@ -0,0 +1,33 @@
+use Mix.Config
+
+config :digital_public_works, DigitalPublicWorksWeb.Endpoint,
+  url: [scheme: "https", host: "staging.digitalpublicworks.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json"
+
+config :logger, level: :info
+
+
+database_url =
+  System.get_env("DATABASE_URL") ||
+    raise """
+    environment variable DATABASE_URL is missing.
+    For example: ecto://USER:PASS@HOST/DATABASE
+    """
+
+config :digital_public_works, DigitalPublicWorks.Repo,
+  ssl: true,
+  url: database_url,
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
+
+config :digital_public_works, DigitalPublicWorksWeb.Endpoint,
+  http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
+  secret_key_base: secret_key_base
+
