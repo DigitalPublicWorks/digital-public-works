3ef3ccc04a1500c3a8f907f8719971ddf13e40e3
Nick Gartmann <nick@rokkincat.com>
2020-02-28 14:52:34 -0600

    Match formatting for config in both staging and prod

--- config/staging.exs
+++ config/staging.exs
@@ -7,24 +7,37 @@ config :digital_public_works, DigitalPublicWorksWeb.Endpoint,
 
 config :logger, level: :info
 
-
-database_url = System.fetch_env!("DATABASE_URL") 
+database_url =
+  System.get_env("DATABASE_URL") ||
+    raise """
+    environment variable DATABASE_URL is missing.
+    For example: ecto://USER:PASS@HOST/DATABASE
+    """
 
 config :digital_public_works, DigitalPublicWorks.Repo,
   ssl: true,
   url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  pool_size: String.to_integer(System.get_env("POOL_SIZE", "10"))
 
-secret_key_base = System.fetch_env!("SECRET_KEY_BASE")
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
 
 config :digital_public_works, DigitalPublicWorksWeb.Endpoint,
-  http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
+  http: [:inet6, port: String.to_integer(System.get_env("PORT", "4000"))],
   secret_key_base: secret_key_base
 
 config :digital_public_works, DigitalPublicWorksWeb.Mailer,
   adapter: Bamboo.SendGridAdapter,
   sandbox: true,
-  api_key: System.fetch_env!("SENDGRID_API_KEY"),
   hackney_opts: [
     recv_timeout: :timer.minutes(1)
-  ]
+  ],
+  api_key:
+    System.get_env("SENDGRID_API_KEY") ||
+      raise("""
+      environment variable SENDGRID_API_KEY is missing.
+      """)
