b3802a076624873777864a25b1084e60de972b58
Nick Gartmann <nick@rokkincat.com>
2020-02-28 14:35:26 -0600

    Move all to fetch_env for staging

--- config/staging.exs
+++ config/staging.exs
@@ -8,24 +8,14 @@ config :digital_public_works, DigitalPublicWorksWeb.Endpoint,
 config :logger, level: :info
 
 
-database_url =
-  System.get_env("DATABASE_URL") ||
-    raise """
-    environment variable DATABASE_URL is missing.
-    For example: ecto://USER:PASS@HOST/DATABASE
-    """
+database_url = System.fetch_env!("DATABASE_URL") 
 
 config :digital_public_works, DigitalPublicWorks.Repo,
   ssl: true,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
-secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
-    raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
+secret_key_base = System.fetch_env!("SECRET_KEY_BASE")
 
 config :digital_public_works, DigitalPublicWorksWeb.Endpoint,
   http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
