9c6cb9c6cd97ecf08571b9c47497aad89209baba
Steve <steve@abstractcoder.com>
2020-03-02 14:29:21 -0600

    * New dashboard view for logged in users * Changed View Projects to Search Projects * Search Project screen sorts featured projects to the top by default * Search Project screen now uses full text search * Moved default row and col from layout and to view templates * Show featured badge on featured projects * Clicking anywhere on project card will take user to project page * Refactored parts of project show screen to rely less on floats * Modified list_posts to accept a list of projects * Switched from container to container-fluid in layout to better support screen real estate

--- lib/digital_public_works_web/controllers/page_controller.ex
+++ lib/digital_public_works_web/controllers/page_controller.ex
@@ -1,18 +1,24 @@
 defmodule DigitalPublicWorksWeb.PageController do
   use DigitalPublicWorksWeb, :controller
-  alias DigitalPublicWorks.Projects
+  alias DigitalPublicWorks.{Projects, Posts}
 
-  def index(conn, _params) do
+  def index(%{assigns: %{current_user: current_user}} = conn, _params) when current_user != nil do
     owned_projects = Projects.list_owned_projects(conn.assigns.current_user)
     followed_projects = Projects.list_followed_projects(conn.assigns.current_user)
-    featured_projects = Projects.list_featured_projects
 
-    featured_projects = featured_projects -- followed_projects
-    featured_projects = featured_projects -- owned_projects
+    posts = Posts.list_posts(owned_projects ++ followed_projects)
 
     conn
     |> assign(:owned_projects, owned_projects)
     |> assign(:followed_projects, followed_projects)
+    |> assign(:posts, posts)
+    |> render("index_user.html")
+  end
+
+  def index(conn, _params) do
+    featured_projects = Projects.list_featured_projects
+
+    conn
     |> assign(:featured_projects, featured_projects)
     |> render("index.html")
   end
