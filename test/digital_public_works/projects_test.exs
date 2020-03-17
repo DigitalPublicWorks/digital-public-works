a8bd5f1b6fd312c2fedc41adcdd9d64bd1134582
Steve Johnson <steve@abstractcoder.com>
2020-03-17 11:42:20 -0500

    Fixed failing test

--- test/digital_public_works/projects_test.exs
+++ test/digital_public_works/projects_test.exs
@@ -113,7 +113,7 @@ defmodule DigitalPublicWorks.ProjectsTest do
       assert Projects.is_user?(project, user) == false
 
       Projects.add_user(project, user)
-      assert Ecto.assoc(user, :joined_projects) |> Repo.all == [project]
+      assert Ecto.assoc(user, :joined_projects) |> Repo.all |> Repo.preload(:user) == [project]
       assert Projects.is_user?(project, user) == true
 
       Projects.remove_user(project, user)
