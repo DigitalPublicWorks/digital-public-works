ca7ce89c8e10c68fc2c27a56ef8d1212f7d2bd90
Steve <steve@abstractcoder.com>
2020-03-19 12:06:58 -0500

    Made email column case insensitive

---
+++ priv/repo/migrations/20200319170128_change_email_type_to_citext.exs
@@ -0,0 +1,17 @@
+defmodule DigitalPublicWorks.Repo.Migrations.ChangeEmailTypeToCitext do
+  use Ecto.Migration
+
+  def up do
+    execute "CREATE EXTENSION citext"
+    alter table(:users) do
+      modify :email, :citext, null: false
+    end
+  end
+
+  def down do
+    alter table(:users) do
+      modify :email, :string, null: false
+    end
+    execute "DROP EXTENSION citext"
+  end
+end
